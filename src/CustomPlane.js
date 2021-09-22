d7007d9f075bd907469f0410b95fe5350408efb6
David B <59053870+David-Beale@users.noreply.github.com>
2021-09-22 18:24:54 +0100

    add mandelbrot

--- src/CustomPlane.js
+++ src/CustomPlane.js
@@ -8,48 +8,49 @@ const _VS = `
   }
   `;
 const _FS = `
-  precision mediump float;
+  precision highp float;
 
-  uniform vec2 mouse;
   uniform vec2 resolution;
-  uniform int atomsNum;
-  uniform vec2 atoms [10];
-  uniform vec2 speed [10];
-  uniform float frames;
+  uniform float offset;
+  uniform vec2 center;
+  uniform float scale;
 
 
   void main() {
-    vec2 xy = gl_FragCoord.xy / resolution.xy;
-    // float d = distance(mouse, xy);
-    // float c = sin(d*100.0);
+    double test = 0.0;
+    float maxN = 250.0;
+    float zoom = 2.0;
+    float a = ((gl_FragCoord.x - offset) / resolution.x - 0.5) * scale + center.x;
+    float b = (gl_FragCoord.y / resolution.y - 0.5) * scale + center.y;
 
-    vec2 newAtoms [10];
-     
-    for(int i = 0; i < atomsNum; i++){
-      newAtoms[i] = atoms[i] + speed[i] * sin(frames);
-    }
+    float ca = a;
+    float cb = b;
 
+    float z = 0.0;
+    
+    float n = 0.0;
+    for (; n < maxN; n++){
+      float a2 = a * a -  b * b;
+      float b2 = 2.0 * a * b;
 
-    float s = 0.0;
+      a = a2 + ca;
+      b = b2 + cb;
 
-    for(int i = 0; i < atomsNum; i++){
-      float d = distance(xy, newAtoms[i]);
-      s += exp(-pow(d * 5.0, 2.0));
-    }
-
-    float c = sin(s * 50.0);
-
-    vec3 cl = vec3(1.0-c, s/1.5, c);
-
-    for(int i = 0; i < atomsNum; i++){
-      float d = distance(xy, newAtoms[i]);
-      if(d < 0.01){
-        cl = vec3(1.0, 1.0, 0);
+      if(abs(a2 + b2) > 4.0){
+        break;
       }
     }
 
+    float brightness = sqrt(n / maxN);
+    if(n == maxN) brightness = 0.0;
 
-    gl_FragColor = vec4(cl, 1.0);
+
+    gl_FragColor = vec4(brightness, brightness, brightness, 1.0);
+    
+    // float c = 0.0;
+    // if(a <0.0) c = 1.0;
+    // gl_FragColor = vec4(c, c, c, 1.0);
+
   }
   `;
 const num_atoms = 10;
@@ -62,24 +63,47 @@ for (let i = 0; i < 2 * num_atoms; i++) {
 export default function CustomPlane() {
   const shader = useRef();
   const counter = useRef(0);
-
+  const scale = useRef(0.0001);
+  const targetScale = useRef(0.0001);
+  const center = useRef([0.2510190964592524, -0.00001057574347854329]);
+  const targetCenter = useRef(center.current);
   useFrame(() => {
-    counter.current += 0.01;
-    shader.current.uniforms.frames.value = counter.current;
+    center.current[0] += (targetCenter.current[0] - center.current[0]) * 0.01;
+    center.current[1] += (targetCenter.current[1] - center.current[1]) * 0.01;
+    scale.current += (targetScale.current - scale.current) * 0.01;
+    // console.log(center.current);
+    // counter.current += 0.01;
+    shader.current.uniforms.center.value = center.current;
+    shader.current.uniforms.scale.value = scale.current;
+    // shader.current.uniforms.frames.value = counter.current;
   }, []);
-
+  const onPointerDown = (e) => {
+    const offset = (window.innerWidth - window.innerHeight) / 2;
+    const px = (e.clientX - offset) / window.innerHeight;
+    const py = e.clientY / window.innerHeight;
+    const dx = (px - 0.5) * scale.current;
+    const dy = (0.5 - py) * scale.current;
+    targetCenter.current = [center.current[0] + dx, center.current[1] + dy];
+    targetScale.current = scale.current * 0.1;
+    console.log(scale.current);
+  };
   return (
-    <mesh position={[0, 0, 0]}>
+    <mesh position={[0, 0, 0]} onPointerDown={onPointerDown}>
       <planeBufferGeometry args={[window.innerHeight, window.innerHeight]} />
       <shaderMaterial
         ref={shader}
         uniforms={{
           mouse: { value: [0, 0] },
           resolution: { value: [window.innerHeight, window.innerHeight] },
-          atomsNum: { value: num_atoms },
-          atoms: { value: l },
-          speed: { value: s },
-          frames: { value: 0 },
+          offset: {
+            value: (window.innerWidth - window.innerHeight) / 2,
+          },
+          center: {
+            value: center.current,
+          },
+          scale: {
+            value: scale.current,
+          },
         }}
         vertexShader={_VS}
         fragmentShader={_FS}
